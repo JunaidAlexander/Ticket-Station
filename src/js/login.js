bd47e5bea564b5ebaf1c1e8f4d98a833597400c9
Junaid Alexander <junaidalexander76@gmail.com>
2023-09-13 00:22:58 -0700

    fixed my login and add the links to the museum
    Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

---
+++ src/js/login.js
@@ -0,0 +1,39 @@
+
+class Login {
+  constructor() {
+    // Initialize Firebase UI and start the authentication process.
+    this.ui = new firebaseui.auth.AuthUI(auth);
+    this.setupAuthentication();
+  }
+
+  // Set up the authentication options and callbacks.
+  setupAuthentication() {
+    this.ui.start("#firebaseui-auth-container", {
+      signInOptions: [
+        {
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          requireDisplayName: false
+        }
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult: this.onSignInSuccess.bind(this)
+      }
+    });
+  }
+
+  // Callback function for successful authentication.
+  onSignInSuccess(authResult, redirectUrl) {
+    // Access the user information.
+    const user = authResult.user;
+
+    // Redirect to "index.html" after successful authentication.
+    this.redirectToIndexPage();
+  }
+
+  // Redirect to "index.html".
+  redirectToIndexPage() {
+    window.location.href = "events.html";
+  }
+}
+
+const app = new Login();
